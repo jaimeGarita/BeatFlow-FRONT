569c7e22271b06c0bc6d5b0822d11eb3f81af567
jaimeGarita <jaime.garita.camargo@gmail.com>
2023-06-10 23:09:39 +0200

    Añadidos nuevos estilos, modo dark entero

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
-    <router-view/>
+    <navbar></navbar>
+    <router-view />
   </div>
 </template>
-
+<script>
+import Navbar from './components/navbar/Navbar.vue';
+import './styles/main.css'
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 100%;
   width: 100%;
-}
-html{
-  height: 100%;
-  width: 100%;
-}
-body{
-  height: 99%;
-  width: 99%;
 }
 
 </style>
--- src/components/loginForm/LoginForm.vue
+++ src/components/loginForm/LoginForm.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="login-container">
-        <el-card class="login-card">
-            <div slot="header" class="clearfix">
-                <span>INICIO</span>
+        <el-card class="login-card transparent-card container">
+            <div slot="header" class="clearfix ">
+                <h1>BeatFlow</h1>
             </div>
             <el-form ref="loginForm" :model="loginForm" :rules="loginRules">
-                <el-form-item label="Usuario" prop="username">
+                <el-form-item label="Usuario" prop="username" label-class="custom-label">
                     <el-input v-model="loginForm.username"></el-input>
                 </el-form-item>
                 <el-form-item label="Contraseña" prop="password">
                     <el-input type="password" v-model="loginForm.password"></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" @click="login">Ingresar</el-button>
+                    <el-button type="primary" @click="login" class="button">Ingresar</el-button>
                 </el-form-item>
             </el-form>
         </el-card>
@@ -23,6 +23,7 @@
 
 import LoginFormJS from "./LoginForm";
 import './LoginForm.css'
+import '../../styles/main.css'
 
 export default {
     ...LoginFormJS
